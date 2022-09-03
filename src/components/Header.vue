1857ae0052984999947f69ae45b4a431dfaa4ae7
MohanManirul <fiforeg@gmail.com>
2022-09-03 12:06:25 +0600

    footer created

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,30 +4,30 @@
     <hr>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div class="container-fluid">
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <router-link class="nav-link" to="/">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/contact_list">Contact List</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/add_contact">Add Contact</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/about_us">About Us</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/vission">Vission</router-link>
-        </li>
-      </ul>
-    </div>
+          <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarColor01">
+              <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                  <router-link class="nav-link" to="/">Home</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link class="nav-link" to="/contact_list">Contact List</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link class="nav-link" to="/add_contact">Add Contact</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link class="nav-link" to="/about_us">About Us</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link class="nav-link" to="/vission">Vission</router-link>
+                </li>
+              </ul>
+            </div>
 
-   <p>fdfdf</p>
-  </div>
-</nav>
+          <p>fdfdf</p>
+          </div>
+      </nav>
     </div>
 
 </header>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue"; // imports only createApp from vue library
 import Vuex from 'vuex'
 import App from "./App.vue"; // linking vue external page
 import routes from './routes/routes' ;
-require("bootstrap")
+
 import "bootstrap/dist/css/bootstrap.css"
 
 createApp(App).use(routes).mount('#app')
