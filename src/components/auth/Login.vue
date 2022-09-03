5392f3b08fef62f51b0776ef776b782ac58f2664
MohanManirul <fiforeg@gmail.com>
2022-09-03 13:14:07 +0600

    login

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -9,7 +9,6 @@
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Email address</label>
                                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Password</label>
@@ -19,7 +18,10 @@
                                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
                                 <label class="form-check-label" for="exampleCheck1">Check me out</label>
                             </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <div class="text-end">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                            
                         </form> 
                     </div>
                </div>
