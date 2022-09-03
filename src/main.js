bd9f1a0df174da3c1f914456d207042333c447bb
MohanManirul <fiforeg@gmail.com>
2022-09-03 20:05:21 +0600

    dd

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from "vue"; // imports only createApp from vue library
 import App from "./App.vue"; // linking vue external page
+import Vuex from 'vuex'
 import routes from './routes/routes' ;
 import "bootstrap/dist/css/bootstrap.css"
 
 createApp(App).use(routes).mount('#app')
 
+export default new Vuex.Store({
+    state:{},
+    getters:{},
+    mutations:{},
+    actions:{
+        login(){
+          console.log('test login...')  
+        }
+    },
+    modules:{}
+})
