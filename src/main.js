fb10f4709656f3f7e83f2a2e568a6ac59b90bd3f
MohanManirul <fiforeg@gmail.com>
2022-09-05 22:35:24 +0600

    dzzdd

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue"; // imports only createApp from vue library
 import App from "./App.vue"; // linking vue external page'
 import routes from './routes/routes' ;
+import './store/index';
 import "bootstrap/dist/css/bootstrap.css"
 
 createApp(App).use(routes).mount('#app')
