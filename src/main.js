1110075df384dadbc1564d522b246f93866d42e6
MohanManirul <fiforeg@gmail.com>
2022-09-09 22:34:30 +0600

    registration api with passport

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue"; // linking vue external page'
 import routes from './routes/routes' ;
 import "bootstrap/dist/css/bootstrap.css"
 import store from './store/index'
-
+import "./assets/scss/app.scss";
 
 const app = createApp(App)
 // use the store in the App
